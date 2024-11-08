8d7007e5719bc4db9fe48956fea301ea0ccc7729
adamshoaib <akareems@asu.edu>
2024-11-08 15:30:00 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [showLockModal, setShowLockModal] = useState(false);
+  // useEffect(() => {
+  //   const handleBeforeUnload = (event) => {
+  //     event.preventDefault();
+  //     event.returnValue = "Hello world";
+  //   };
+
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, []);
+  const [sessions, setSessions] = useState("");
+
+  const TAB_ID = Math.random().toString(36).substring(2, 15); // unique ID for each tab
+
+  useEffect(() => {
+    // Add this tab to open tabs in localStorage
+    const openTabs = JSON.parse(localStorage.getItem("openTabs") || "[]");
+    localStorage.setItem("openTabs", JSON.stringify([...openTabs, TAB_ID]));
+
+    const handleBeforeUnload = (event) => {
+      const remainingTabs = JSON.parse(
+        localStorage.getItem("openTabs") || "[]"
+      ).filter((id) => id !== TAB_ID);
+      localStorage.setItem("openTabs", JSON.stringify(remainingTabs));
+
+      if (remainingTabs.length === 0) {
+        event.preventDefault();
+        alert("Im closing sorry");
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      // handleBeforeUnload(); // Ensure this runs when React component unmounts
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <a href="www.google.com">Link</a>
+      <div>
+        <h1>{sessions}</h1>
+      </div>
+      <button
+        onClick={() => {
+          setShowLockModal(true);
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
